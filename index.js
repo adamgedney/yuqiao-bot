58ea3da9d4c29e194d6f2940f5d31f593ad5f460
Adam Gedney <adam@olympussat.com>
2018-05-07 10:33:50 -0400

    UPDATED bot to inclue eli and adam bot

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-(function(timeInt) {
+(function() {
   const slack = require('./slack.js');
-  const statement = `in ${timeInt}`;
+  const bot = process.argv[process.argv.length - 2];
+  const timeInt = process.argv[process.argv.length - 1];
+  let statement = '';
+  let channel = undefined;
+  let icon = '';
+  let username = "";
   
-  slack.sendMessage(statement, function(res) {
-    console.log(statement, res);
+  switch (bot) {
+  	case 'adam' :
+  		statement = `How was Friday? Did you meet the goal? How about today? What are you aiming for?`;
+  		channel = "G9NL1RC85";
+  		icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Circle-icons-rocket.svg/150px-Circle-icons-rocket.svg.png";
+  		username = "adamgedney";
+  		break;
+  	case 'eli' :
+  		statement = `in at ${timeInt}`;
+  		channel = "i-will-be-late";
+  		icon = "https://lh3.googleusercontent.com/-pqGTgsaPkpI/AAAAAAAAAAI/AAAAAAAAALM/UwsL7ZdDjNE/photo.jpg?sz=328";
+  		username = "eli";
+  		break;
+  	case 'yuqiao' :
+  	default :
+  		statement = `in ${timeInt}`;
+  		icon = "https://image.ibb.co/e6Je9S/Screen_Shot_2018_05_04_at_11_31_00_AM.png";
+  		username = "yuqiao";
+  }
+  
+  slack.sendMessage(statement, channel, icon, username, function(res) {
+    console.log(statement, channel, icon, username);
   });
   
   return statement;
-})(15);
+})();
--- slack.js
+++ slack.js
@@ -2,7 +2,7 @@ module.exports = {
   /**
    * Example POST: payload={"text": "This is a line of text in a channel.\nAnd this is another line of text."}
    */
-  sendMessage: function(message, cb) {
+  sendMessage: function(message, channel, icon, username, cb) {
     var Slack = require('slack-node');
     const config = require('./config.js');
 
@@ -10,9 +10,9 @@ module.exports = {
     slack.setWebhook(config.SLACK_WEBHOOK_URI);
     
     slack.webhook({
-      // channel: "@adamgedney",
-      username: "yuqiao",
-      icon_emoji: "https://image.ibb.co/e6Je9S/Screen_Shot_2018_05_04_at_11_31_00_AM.png",
+      channel: channel,
+      username: username,
+      icon_emoji: icon,
       text: message
     }, function(err, response) {
       cb(response);
