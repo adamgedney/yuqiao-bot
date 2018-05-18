5d9a8ca61f9c1eff14bedee17292bac4e729c813
Adam Gedney <adam@olympussat.com>
2018-05-18 10:26:31 -0400

    ADDED erica bot and eriqiao

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   const bot = process.argv[process.argv.length - 2];
   const timeInt = process.argv[process.argv.length - 1];
   let statement = '';
-  let channel = undefined;
+  let channel = undefined;// Default channel is i-will-be-yuqiao
   let icon = '';
   let username = "";
   
@@ -13,25 +13,40 @@
   		statement = new Date().getDay() === 1 
 	  		? `How was Friday? Did you meet the goal? How about today? What are you aiming for?` 
 	  		: `How did it go yesterday? Did you meet your goals? What's the goal for today?`;
-  		channel = "G9NL1RC85";
+  		channel = "G9NL1RC85";// @adamgedney, @erica, @marcopolo25
   		icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Circle-icons-rocket.svg/150px-Circle-icons-rocket.svg.png";
   		username = "adamgedney";
   		break;
   	case 'eli' :
   		statement = `in at ${timeInt}`;
-  		channel = "i-will-be-late";
+  		// channel = "D2B8XRKN2";
+  		channel = "i-will-b-late";
   		icon = "https://lh3.googleusercontent.com/-pqGTgsaPkpI/AAAAAAAAAAI/AAAAAAAAALM/UwsL7ZdDjNE/photo.jpg?sz=328";
   		username = "eli";
   		break;
+  	case 'erica' :
+  		statement = `${timeInt}`;
+  		channel = "D8PSA1T8V";// @adam @sean
+  		// channel = "i-will-b-late";
+  		icon = "https://image.ibb.co/bQnUGJ/Screen_Shot_2018_05_18_at_10_13_11_AM.png";
+  		username = "erica";
+  		break;
+    case 'eriqiao' :
+      const ints = [15, 1];
+  		statement = `${ints[Math.ceil(Math.random() * ints.length) - 1]}`;
+  		// channel = "D8PSA1T8V";// @adam @sean
+  		icon = "https://image.ibb.co/bQnUGJ/Screen_Shot_2018_05_18_at_10_13_11_AM.png";
+  		username = "eriqiao";
+  		break;
   	case 'yuqiao' :
-  	default :
-  		statement = `in ${timeInt}`;
+    default :
+  		statement = `${timeInt}`;
   		icon = "https://image.ibb.co/e6Je9S/Screen_Shot_2018_05_04_at_11_31_00_AM.png";
   		username = "yuqiao";
   }
-  
+
   slack.sendMessage(statement, channel, icon, username, function(res) {
-    console.log(statement, channel, icon, username);
+    console.log(statement, channel, icon, username, res);
   });
   
   return statement;
